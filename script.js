2e5087164f568dae22e270c903687392d49a8df0
shaunsim <shaunsim25@gmail.com>
2022-04-22 21:35:23 +0800

    Add password matching

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+function triggersAfterInteraction(e){
+    this.classList.add("displayPseudoState");
+}
+
+function checkPasswords(e){
+    if (password.value === confirmPassword.value){
+        confirmPassword.classList.remove('confirmationError');
+    }
+    else{
+        confirmPassword.classList.add('confirmationError');
+    }
+}
+
 let inputElements = document.querySelectorAll('input');
+let password = document.querySelector('#password');
+let confirmPassword = document.querySelector('#confirm-password');
 
 for (let element of inputElements){
-    element.addEventListener('click',addClass);
-    element.addEventListener('focus',addClass);
+    element.addEventListener('click',triggersAfterInteraction);
+    element.addEventListener('focus',triggersAfterInteraction);
 }
 
-function addClass(e){
-    this.setAttribute("class", "displayPseudoState");
-}
+password.addEventListener('change',checkPasswords);
+confirmPassword.addEventListener('change',checkPasswords);
